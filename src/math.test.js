a2056238fb20928108fb9630d8b175f01a8e5399
gpmarchi <gustavomarchi@gmail.com>
2022-06-03 17:59:32 -0300

    feat: add subtract operation

--- src/math.test.js
+++ src/math.test.js
@@ -1,5 +1,9 @@
-const sum = require("./math.js")
+const { sum, subtract } = require("./math.js")
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+});
+
+test('adds 1 - 1 to equal 0', () => {
+  expect(subtract(1, 1)).toBe(0)
 });
